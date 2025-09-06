4ab895677bf3a5a6e4508906c27ba30b3e2f299a
mooojn <moojntariq@gmail.com>
2025-09-06 04:38:45 -0700

    Update fontSize

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Heading = ({ head = 'heading', end = '', description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est laborum quo, odit dignissimos voluptatibus minus magnam reprehenderit repellat nam quasi?' }) => {
   return (
     <>
-      <div style={{ textAlign: "center", fontSize: "40px", fontFamily: "Consolas", color: "var(--primary)" }}>
+      <div style={{ textAlign: "center", fontSize: "20px", fontFamily: "Consolas", color: "var(--primary)" }}>
         <h1 style={{}}>
           {head}{" "}
           <span
