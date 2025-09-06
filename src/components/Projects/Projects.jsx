6308c8b0758165897d418c42e58e5859766e7b50
mooojn <moojntariq@gmail.com>
2025-09-06 04:59:07 -0700

    Add projects css to cnter cards and add gap

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Card from './Card/Card.jsx'
 import Heading from '../../Heading.jsx'
+import './Projects.css'
 
 const Projects = () => {
   return (
     <div>
       <Heading head='Recent' end='Projects' description='I enjoy building side projects and constantly challenge myself apart from my actual work :)'></Heading>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
+      <div className="cards">
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </div>
     </div>
   )
 }
