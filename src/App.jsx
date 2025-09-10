6d64471c5663e329638560e9c57c05260d7a95ad
mooojn <moojntariq@gmail.com>
2025-09-10 09:21:50 -0700

    Add blob with header

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Header from './components/Header/Header.jsx'
 import Hero from './components/Hero/Hero.jsx'
 import Projects from './components/Projects/Projects.jsx'
+import Blob from './components/Blob/Blob.jsx'
 
 const App = () => {
   return (
     <>
+      <Blob />
       <Header></Header>
       <Hero></Hero>
       <Projects></Projects>
---
+++ src/components/Blob/Blob.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Blob.css";
+
+const Blob = () => {
+  return (
+    <div className="comp-blob-wrapper">
+      <div className="comp-blob"></div>
+    </div>
+  );
+};
+
+export default Blob;
