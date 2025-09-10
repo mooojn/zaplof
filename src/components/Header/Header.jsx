95e112127d64ab1e6eaa8072589e94b59a0f1e6f
mooojn <moojntariq@gmail.com>
2025-09-10 09:43:32 -0700

    Add sticky nav-items, Fixed action and logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,18 +7,18 @@ const Header = () => {
         <>
             <div className="header">
                 <div className="logo"><img src={logo} alt="Logo" /></div>
-                <div className="nav">
-                    <ul className="nav-items">
-                        <li>Home</li>
-                        <li>Contact</li>
-                        <li>About</li>
-                        <li>Services</li>
-                        <li>Projects</li>
-                    </ul>
-                </div>
                 <button className="call-to-action">Get Started</button>
             </div>
 
+            <div className="nav">
+                <ul className="nav-items">
+                    <li>Home</li>
+                    <li>Contact</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Projects</li>
+                </ul>
+            </div>
         </>
     )
 }
