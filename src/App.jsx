078da2b569608c795418dc85e07583e5e6118774
mooojn <moojntariq@gmail.com>
2025-09-16 20:11:01 +0500

    Add margin-bottom bw each component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from './components/Header/Header.jsx'
 import Hero from './components/Hero/Hero.jsx'
 import Projects from './components/Projects/Projects.jsx'
 import Blob from './components/Blob/Blob.jsx'
+import Experience from './components/Experience/Experience.jsx'
+import Skills from './components/Skills/Skills.jsx'
 
 const App = () => {
   return (
@@ -10,7 +12,11 @@ const App = () => {
       <Blob />
       <Header></Header>
       <Hero></Hero>
+    <div className="components">
       <Projects></Projects>
+      <Experience></Experience>
+      <Skills></Skills>
+    </div>
     </>
   )
 }
