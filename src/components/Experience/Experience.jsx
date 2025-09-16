e162b40126b3016e605750a2790929ffe6fd0f1c
mooojn <moojntariq@gmail.com>
2025-09-16 20:12:21 +0500

    Add Experience component boiler

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Heading from '../../Heading'
+import './Experience.css'
+
+const Experience = () => {
+    return (
+        <div>
+            <Heading head='My' end='Experience' description='Delivering Results: A Tech Professional`s Journey'></Heading>
+
+        </div>
+    )
+}
+
+export default Experience
