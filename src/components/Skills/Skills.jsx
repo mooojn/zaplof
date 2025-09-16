0e6daff71ee5d3f0bf3ee5a81ae25f8b9b872918
mooojn <moojntariq@gmail.com>
2025-09-16 20:12:30 +0500

    Add Skills component boiler

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Heading from '../../Heading'
+import './Skills.css'
+
+const Skills = () => {
+    return (
+        <div>
+            <Heading head='' end='Skills' description='Some of the tools I play with, Flexible to learn anything though :)'></Heading>
+
+        </div>
+    )
+}
+
+export default Skills
