bc4c81e68f8b995b1d88a281d5dab52a8afaa4cc
mooojn <moojntariq@gmail.com>
2025-09-20 20:28:32 +0500

    Add Skills comp

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import Heading from '../../Heading'
+import { skills } from '../../data/data.jsx'
 import './Skills.css'
 
 const Skills = () => {
     return (
         <div>
             <Heading head='' end='Skills' description='Some of the tools I play with, Flexible to learn anything though :)'></Heading>
+            <div className="skills-grid">
+                {skills.map((skill, i) => (
+                    <div className="skill-card" key={i}>
+                        <div
+                            className="skill-logo"
+                            style={{ "--skill-color": skill.color }}
+                        >
+                            {skill.icon}
+                        </div>
+                        <div className="skill-info">
+                            <p className="percent">{skill.percent}</p>
+                            <p className="name">{skill.name}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
 
         </div>
     )
