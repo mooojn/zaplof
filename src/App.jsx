b502699c75d6b46b9930d0a243d077134f2d9137
mooojn <moojntariq@gmail.com>
2025-10-02 18:24:51 +0500

    Add separate layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
-import Header from './components/Header/Header.jsx'
-import Hero from './components/Hero/Hero.jsx'
-import Projects from './components/Projects/Projects.jsx'
-import Blob from './components/Blob/Blob.jsx'
-// import Experience from './components/Experience/Experience.jsx'
-import Services from './components/Services/Services.jsx'
+import { components } from './data/layout.jsx'
 
 const App = () => {
   return (
     <>
-      <Blob />
-      <Header></Header>
-      <Hero></Hero>
-    <div className="components">
-      <Projects></Projects>
-      {/* <Experience></Experience> */}
-      <Services></Services>
-    </div>
+      {components.map((comp, idx) =>
+        comp.id === null ? (
+          <React.Fragment key={idx}>{comp.component}</React.Fragment>
+        ) : (
+          <div key={idx} id={comp.id}>
+            {comp.component}
+          </div>
+        )
+      )}
     </>
+
   )
 }
 
